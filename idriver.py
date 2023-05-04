525ad1055a032c8444ee583989188e9d1a2c201b
fansirsqi <2104898527@qq.com>
2023-05-04 12:02:08 +0800

    修改安装驱动脚本

--- idriver.py
+++ idriver.py
@@ -1,20 +1,39 @@
-'''
-Author: FansirQ 2104898527@qq.com
-Date: 2023-05-01 01:43:58
-LastEditors: FansirQ 2104898527@qq.com
-LastEditTime: 2023-05-01 13:31:49
-FilePath: \pt\installedge.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 from selenium import webdriver
 
-#for Edge
+# for Edge
+from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-#for Chrome
+# for Chrome
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver_chrome = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver_edge = webdriver.Edge(EdgeChromiumDriverManager().install())
-exit()
+# selenium 4  Chromium
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.core.utils import ChromeType
+
+# firefox
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+# IE
+from selenium.webdriver.ie.service import Service as IEService
+from webdriver_manager.microsoft import IEDriverManager
+
+
+if __name__ == "__main__":
+    print('选择您的浏览器:\n1. firefox\n2. IE\n3. Chromium\n4. Chrome\n5. Edge\n')
+    select = input(':')
+    if select == '1':
+        driver_firefox = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif select == '2':
+        driver_ie = webdriver.Ie(service=IEService(IEDriverManager().install()))
+    elif select == '3':
+        driver_Chromium = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    elif select == '4':
+        driver_chrome = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+    elif select == '5':
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    else:
+        print('输入有误!,请您重新运行脚本选择安装')
